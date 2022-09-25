f9d868afdc2ce526d04436e781675e7a72b9069b
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2022-09-26 04:53:45 +0530

    added files

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 ScrollReveal({ reset: true }).reveal('.main_quote', { delay: 500 });
 ScrollReveal({ reset: true }).reveal('#hashtags', { delay: 800 });
 ScrollReveal({ reset: true }).reveal('.kit', { interval: 400, delay: 1200 });
+ScrollReveal({ reset: true }).reveal('.grid_left_box', { interval: 400, delay: 2400 });
