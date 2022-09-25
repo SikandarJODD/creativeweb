8bb577c5438d4cf67e9f441ea016a655fc248f3e
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2022-09-26 00:44:34 +0530

    Starter Code

---
+++ index.js
@@ -0,0 +1,3 @@
+ScrollReveal({ reset: true }).reveal('.main_quote', { delay: 500 });
+ScrollReveal({ reset: true }).reveal('#hashtags', { delay: 800 });
+ScrollReveal({ reset: true }).reveal('.kit', { interval: 400, delay: 1200 });
